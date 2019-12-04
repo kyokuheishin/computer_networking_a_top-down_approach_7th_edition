93fd9c0d2e83f07969c77d99540a0091eae6ae2e
kyokuheishin <kyokuheishin@gmail.com>
2019-12-04 20:12:09 +0800

    Initial commit

---
+++ web_server.py
@@ -0,0 +1,35 @@
+from socket import *
+serverSocket = socket(AF_INET,SOCK_STREAM)
+socket_port = 12000
+serverSocket.bind(('',socket_port))
+serverSocket.listen(1)
+
+while True:
+    print("ready to serve")
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        print(message)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        # Send a valid HTTP header
+        header = """
+         HTTP/1.1 200 OK
+        Connection: close
+        Content-Length: %d
+        Content-Type: text/html
+
+        
+        """ % len(outputdata)
+        connectionSocket.send(header.encode())
+
+        for i in range(0,len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        
+        connectionSocket.close()
+
+    except IOError:
+        header = " HTTP/1.1 404 Not Found"
+        connectionSocket.send(header.encode())
+        connectionSocket.close()
